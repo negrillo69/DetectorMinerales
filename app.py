24bcff5616da8570ed5b3162f24702b31acb88ff
negrillo69 <negrillo.c2@gmail.com>
2024-10-06 17:05:03 -0300

    conecciones

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, render_template
 from pymongo import MongoClient
 import random
-import models.model
+from models.model import MineralModel
 
 app = Flask(__name__)
 
@@ -19,22 +19,28 @@ def upload():
     if 'image' not in request.files:
         return 'No file part', 400
     file = request.files['image']
+
     
     # Verificar si se seleccionó un archivo
     if file.filename == '':
         return 'No selected file', 400
 
     # Aquí puedes realizar validaciones adicionales si es necesario
+    image_path = f'static/{file.filename}'
+    file.save(image_path)
+    model_path = 'models/best_model.keras'
+    model = MineralModel(model_path)
+    resultado = model.predict(image_path)
+    print(resultado)
 
     # Obtener un mineral aleatorio de la base de datos
-    minerals = list(minerals_collection.find())  # Asegúrate de que existe esta colección
+    minerals = minerals_collection.find_one({"nombre": resultado})  # Asegúrate de que existe esta colección
     if not minerals:
         return 'No hay minerales en la base de datos', 404
-
-    random_mineral = random.choice(minerals)
+    print(minerals)
     
 
-    return render_template('results.html', mineral=random_mineral)
+    return render_template('results.html', mineral=minerals)
 
 if __name__ == '__main__':
     app.run(debug=True)
